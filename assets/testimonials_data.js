73cc71d370e26883edd28d99838504bc95e584d4
Euan Chua <github@eu4nza.me>
2025-12-06 16:08:18 +0800

    added testimonials data.js

---
+++ assets/testimonials_data.js
@@ -0,0 +1,32 @@
+const testimonialsData = [
+  {
+    "name": "Carlette",
+    "position": "Colleague / Peer",
+    "testimonial": "I would recommend Euan because he’s a natural team player and leader. He guides everyone on the team in a way that makes things clearer and easier to understand. He also listens to everyone’s ideas and makes sure no one feels left out. Euan stays calm under pressure, communicates well, and always tries to bring out the best in the people around him. He’s someone you can trust, and he consistently shows dedication and kindness in everything he does.",
+    "image": "../assets/images/testimonials/kai.webp"
+  },
+  {
+    "name": "Leen",
+    "position": "Colleague / Peer",
+    "testimonial": "Euan has exceptional programming ability and demonstrates remarkable speed and efficiency, consistently completing tasks. His foundation is built upon strong technical knowledge and is great at problem-solving, allowing him to rapidly comprehend and apply complex technical concepts. Beyond his impressive skills, he is an excellent colleague when it comes to work, maintaining a positive and highly motivated attitude that significantly elevates the entire team dynamic. Euan is reliable and is driven by an unwavering commitment to producing best-in-class work, even if it’s not his own work—qualities that collectively make him stand out in any setting.",
+    "image": "../assets/images/testimonials/leen.webp"
+  },
+  {
+    "name": "Ling",
+    "position": "Colleague / Peer",
+    "testimonial": "Euan is highly adaptable; he knows how to find information, which is an important skill, especially in the tech field. Moreover, when given a task, he is the type of person who always delivers.",
+    "image": "../assets/images/testimonials/ling.webp"
+  },
+  {
+    "name": "Louis",
+    "position": "Colleague / Peer",
+    "testimonial": "Euan is the type of person who is eager to try new things because he is always curious about how things work, and that curiosity drives him to explore, learn, and take on new challenges. That trait he have allows him to grow, adapt, and go beyond his limits.",
+    "image": "../assets/images/testimonials/louis.webp"
+  },
+  {
+    "name": "Fairyel",
+    "position": "Colleague / Peer",
+    "testimonial": "If I were to recommend him, I'd say that he is knowledgeable about his own work, and is determined to be able to accomplish a task regardless of its difficulty. I have seen him hands on with his activities in different fields, and I observed that he works well under pressure and shows great professionalism. His flexibility and agility to perform his duties is good— rather highly impressive.",
+    "image": "../assets/images/testimonials/eya.webp"
+  }
+];
